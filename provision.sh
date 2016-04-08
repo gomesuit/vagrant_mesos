6ac6d57839112c27a44d41c08c5f921af0dbaaeb
gomesuit <gomesuit@gmail.com>
2016-04-08 10:32:22 +0900

    yum install

--- provision.sh
+++ provision.sh
@@ -26,3 +26,37 @@ git clone https://github.com/gomesuit/provisioner.git
 # run ansbile
 cd /home/vagrant/provisioner
 ansible-playbook -i hosts provisioner.yml
+
+
+yum install autoconf
+
+yum install gcc
+
+yum install gcc-c++
+
+yum install patch
+
+yum install zlib
+yum install zlib-devel
+
+yum install libcurl-devel
+
+yum install apr-devel
+
+yum install subversion-devel
+
+yum install cyrus-sasl-md5
+
+yum install java-1.8.0-openjdk
+yum install java-1.8.0-openjdk-devel
+
+
+
+
+export JAVA_HOME="/etc/alternatives/java"
+
+#wget http://www.apache.org/dist/mesos/0.28.0/mesos-0.28.0.tar.gz
+#tar zxf mesos-0.28.0.tar.gz
+#cd mesos-0.28.0/
+#../configure --prefix=/usr/local/mesos0.28.0
+
